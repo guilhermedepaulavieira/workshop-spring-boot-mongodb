4bdbce16c1c5f83613368ee100575864e7b0db5d
Guilherme de Paula Vieira <guilhermedepaulavieira@hotmail.com>
2022-11-03 15:55:40 -0300

    Getting a user by id

--- src/main/java/com/depaulavieira/workshopmongo/services/UserService.java
+++ src/main/java/com/depaulavieira/workshopmongo/services/UserService.java
@@ -1,12 +1,14 @@
 package com.depaulavieira.workshopmongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.depaulavieira.workshopmongo.domain.User;
 import com.depaulavieira.workshopmongo.repository.UserRepository;
+import com.depaulavieira.workshopmongo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -18,4 +20,10 @@ public class UserService {
 	public List<User> findAll(){
 		return repo.findAll();
 	}
+	
+	public User findById(String id) {
+		Optional<User> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Object not Found"));
+	}
+	
 }
