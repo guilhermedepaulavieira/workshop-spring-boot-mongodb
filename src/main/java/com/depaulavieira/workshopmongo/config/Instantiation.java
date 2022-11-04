a07beda2e540a0e216afe8dca392065c76eeb719
Guilherme de Paula Vieira <guilhermedepaulavieira@hotmail.com>
2022-11-04 16:46:51 -0300

    Projection of author data with DTO

--- src/main/java/com/depaulavieira/workshopmongo/config/Instantiation.java
+++ src/main/java/com/depaulavieira/workshopmongo/config/Instantiation.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Configuration;
 
 import com.depaulavieira.workshopmongo.domain.Post;
 import com.depaulavieira.workshopmongo.domain.User;
+import com.depaulavieira.workshopmongo.dto.AuthorDTO;
 import com.depaulavieira.workshopmongo.repository.PostRepository;
 import com.depaulavieira.workshopmongo.repository.UserRepository;
 
@@ -34,12 +35,12 @@ public class Instantiation implements CommandLineRunner{
 		User maria = new User(null, "Maria Brown", "maria@gmail.com");
 		User alex = new User(null, "Alex Green", "alex@gmail.com");
 		User bob = new User(null, "Bob Grey", "bob@gmail.com");
+
+		userRepository.saveAll(Arrays.asList(maria, alex, bob)); // needed for to have an own ID
 		
-		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Let's travel", "I will travel to São Paulo. Hugs!", maria);
-		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Good Morning", "I woke up happy today!", maria);
-//		Post post3 = new Post(null, sdf.parse("21/03/2018"), "Partiu Viagem", "Vou viajar para São Paulo. Abraços!");
+		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Let's travel", "I will travel to São Paulo. Hugs!", new AuthorDTO(maria));
+		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Good Morning", "I woke up happy today!", new AuthorDTO(maria));
 		
-		userRepository.saveAll(Arrays.asList(maria, alex, bob));
 		postRepository.saveAll(Arrays.asList(post1, post2));
 		
 	}
