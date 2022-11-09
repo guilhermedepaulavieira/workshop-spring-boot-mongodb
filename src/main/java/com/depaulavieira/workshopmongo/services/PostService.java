72cc1c1038a5acf800db6e3ee486022288421954
Guilherme de Paula Vieira <guilhermedepaulavieira@hotmail.com>
2022-11-09 17:24:23 -0300

    Simple query with query methods

--- src/main/java/com/depaulavieira/workshopmongo/services/PostService.java
+++ src/main/java/com/depaulavieira/workshopmongo/services/PostService.java
@@ -1,5 +1,6 @@
 package com.depaulavieira.workshopmongo.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,4 +21,8 @@ public class PostService {
 		Optional<Post> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Object not Found"));
 	}	
+	
+	public List<Post> findByTitle(String text) {
+		return repo.findByTitleContainingIgnoreCase(text);
+	}
 }
