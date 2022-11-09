9b1785ebbdbfa4156b065a5b3cdf3565acb0619f
Guilherme de Paula Vieira <guilhermedepaulavieira@hotmail.com>
2022-11-09 17:31:52 -0300

    Simple query with @Query

--- src/main/java/com/depaulavieira/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/depaulavieira/workshopmongo/repository/PostRepository.java
@@ -3,6 +3,7 @@ package com.depaulavieira.workshopmongo.repository;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.depaulavieira.workshopmongo.domain.Post;
@@ -10,5 +11,8 @@ import com.depaulavieira.workshopmongo.domain.Post;
 @Repository
 public interface PostRepository extends MongoRepository<Post, String> {
 
+	@Query("{ 'Title': { $regex: ?0, $options: 'i' } }")
+	List<Post> searchTitle(String texto);
+	
 	List<Post> findByTitleContainingIgnoreCase(String text);
 }
